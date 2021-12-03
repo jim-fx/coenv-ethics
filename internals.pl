19fffdef63e8ce174e670b468d30f71939a98cb8
jim-fx <>
2021-12-03 11:53:19 +0000

    Deploying to gh-pages from @ jim-fx/coenv-ethics@1cf4b7be0ad0e75a954a1f5a4455fb8cda2cef57 🚀

---
+++ internals.pl
@@ -0,0 +1,26 @@
+# LaTeX2HTML 2019.2 (Released June 5, 2019)
+# Associate internals original text with physical files.
+
+
+$key = q/cite_Cabral2016/;
+$ref_files{$key} = "$dir".q|coenv.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Lengen2021-xe/;
+$ref_files{$key} = "$dir".q|coenv.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:sub1/;
+$ref_files{$key} = "$dir".q|coenv.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:sub2/;
+$ref_files{$key} = "$dir".q|coenv.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:test/;
+$ref_files{$key} = "$dir".q|coenv.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
---
+++ labels.pl
@@ -0,0 +1,57 @@
+# LaTeX2HTML 2019.2 (Released June 5, 2019)
+# Associate labels original text with physical files.
+
+
+$key = q/cite_Cabral2016/;
+$external_labels{$key} = "$URL/" . q|coenv.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Lengen2021-xe/;
+$external_labels{$key} = "$URL/" . q|coenv.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:sub1/;
+$external_labels{$key} = "$URL/" . q|coenv.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:sub2/;
+$external_labels{$key} = "$URL/" . q|coenv.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:test/;
+$external_labels{$key} = "$URL/" . q|coenv.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
+
+# LaTeX2HTML 2019.2 (Released June 5, 2019)
+# labels from external_latex_labels array.
+
+
+$key = q/_/;
+$external_latex_labels{$key} = q|<|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:sub1/;
+$external_latex_labels{$key} = q|1a|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:sub2/;
+$external_latex_labels{$key} = q|1b|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:test/;
+$external_latex_labels{$key} = q|1|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sub_fig:sub1/;
+$external_latex_labels{$key} = q|a|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sub_fig:sub2/;
+$external_latex_labels{$key} = q|b|; 
+$noresave{$key} = "$nosave";
+
+1;
+
